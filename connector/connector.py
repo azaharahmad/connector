1e8cef0dd90f63cf393db3f144488ca681a7ea54
azahar <mr.art64@gmail.com>
2015-07-31 10:20:54 +0800

    fixed Doc string

--- connector/connector.py
+++ connector/connector.py
@@ -13,12 +13,17 @@ FTRACK_CONNECTOR = True
 
 
 class Connector(object):
-    """docstring for connector"""
+    """class for connector
+    Args:
+        user (Optional[str]): user name, and user system username if None
+    """
     def __init__(self, user=None):
         super(Connector, self).__init__()
 
         self.user = user
         self.userDetails = None
+        self.userTasks = None
+
         if not self.user:
             self.user = os.environ['USERNAME']
 
@@ -28,12 +33,20 @@ class Connector(object):
     def setUser(self, value):
         self.user = value
 
-    def connect(self):
+
+
+
+    def connect(self):    
 
         os.environ['LOGNAME'] = self.user
         if FTRACK_CONNECTOR is True:
             print 'Connection Sussceful !'
 
     def getUserDetails(self):
+
+        self.userDetails = ftrack.getUser(self.user)
+        return self.userDetails
+
+    def getUserTasks(self):
         self.userDetails = ftrack.getUser(self.user)
         return self.userDetails
